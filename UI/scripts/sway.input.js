80d1070c97e9756fb13fd2d31085506cdd6660e7
JimAnkrom <arctanone@gmail.com>
2014-08-12 23:42:01 -0400

    POC of UI event collection complete

--- UI/scripts/sway.input.js
+++ UI/scripts/sway.input.js
@@ -2,8 +2,16 @@
  * Created by Jim Ankrom on 7/30/2014.
  *
  * References:
+ * http://www.w3.org/TR/orientation-event/
  * https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Orientation_and_motion_data_explained
  *
+ * X, Y, Z - East, North, Up (off the device)
+ *
+ * Device lying flat horizontal pointing west:
+ *  {alpha: 90,
+       beta: 0,
+       gamma: 0};
+
  * touchstart, touchmove events
  */
 var sway = {
@@ -12,14 +20,14 @@ var sway = {
         // TODO: Orientation
         // TODO: Motion
         // TODO: Touch
-        capabilities: null,
+        capabilities: {},
+        calibration: {},
 
         // TODO: DeviceOrientationEvent.absolute
         // TODO: DeviceOrientationEvent.alpha
         // TODO: DeviceOrientationEvent.beta
         // TODO: DeviceOrientationEvent.gamma,
         // TODO: Browsers may handle this differently, please test.
-        calibration: null,
 
         // TODO: DeviceMotionEvent.acceleration - if data is missing here, try IncludingGravity
         // TODO: DeviceMotionEvent.accelerationIncludingGravity
@@ -29,45 +37,66 @@ var sway = {
 
         // DeviceOrientationEvent handler
         handleOrientationEvent: function (e) {
-            if (!calibration) {
-                this.calibration = e;
+//            if (!calibration) {
+//                // This should always be the user pointing towards their desired start point on the screen!
+//                // We may be able to have them point to "the djs" first, or even run a system calibration to establish where the compass heading is.
+//                this.calibration = e;
+//            }
+            if (sway.debugPanel) {
+                this.renderDebugEvent(sway.debugPanel, e);
             }
+            sway.input.calibration.orientation = e;
         },
         // DeviceMotionEvent handler
         handleMotionEvent: function (e) {
-            this.motion = e;
+            this.calibration.motion = e.acceleration || e.accellerationIncludingGravity || {};
+            this.calibration.rotation = e.rotationRate || {};
+
+            if (sway.debugPanel) {
+                this.renderDebugEvent.call(this, sway.debugPanel, e);
+            }
         },
         handleTouchEvent: function () {
-
         },
         outputToDebug: function () {
-            if (sway.debugPanel) {
-
-            }
+        },
+        renderDebugEvent: function (panel, e) {
+            var o = this.calibration.orientation;
+            var m = this.calibration.motion;
+            var r = this.calibration.rotation;
+            panel.innerHTML = "absolute: " + o.absolute;
+            panel.innerHTML += "<br>alpha: " + o.alpha;
+            panel.innerHTML += "<br>beta: " + o.beta;
+            panel.innerHTML += "<br>gamma: " + o.gamma;
+            panel.innerHTML += "<br>accel X: " + m.x;
+            panel.innerHTML += "<br>accel Y: " + m.y;
+            panel.innerHTML += "<br>accel Z: " + m.z;
+            panel.innerHTML += "<br>rotation alpha: " + r.alpha;
+            panel.innerHTML += "<br>rotation beta: " + r.beta;
+            panel.innerHTML += "<br>rotation gamma: " + r.gamma;
         }
     }
 };
 
-// Detect Browser Capabilities
-if (window.DeviceOrientationEvent) {
-    sway.input.capabilities.orientation = true;
-    window.ondeviceorientation = function (e) {
-        sway.input.handleOrientationEvent.call(sway.input, e);
-    }
-}
-if (window.DeviceMotionEvent) {
-    sway.input.capabilities.motion = true;
-    window.ondevicemotion = function (e) {
-        sway.input.handleMotionEvent.call(sway.input, e);
-    }
-}
-
-// Set up
-window.onload = function () {
-  var element = document.getElementById('debugPanel');
+window.addEventListener('load', function () {
+    alert("Sway v0.13!");
+    var element = document.getElementById('debugPanel');
     if (element) {
         sway.debugPanel = element;
     }
-};
+    // Detect Browser Capabilities
+    if (window.DeviceOrientationEvent) {
+        sway.input.capabilities.orientation = true;
+        window.addEventListener('deviceorientation', function (e) {
+            sway.input.handleOrientationEvent.call(sway.input, e);
+        });
+    }
+    if (window.DeviceMotionEvent) {
+        sway.input.capabilities.motion = true;
+        window.addEventListener('devicemotion', function (e) {
+            sway.input.handleMotionEvent.call(sway.input, e);
+        });
+    }
+});
 
 
--- requirements.md
+++ requirements.md
@@ -1,4 +1,7 @@
-
+# References
+    - http://www.w3.org/TR/orientation-event/
+    
+    
 # Non-Functional Requirements
     ### Front-end must depend on an extremely minimal set of libraries.
 
@@ -22,4 +25,23 @@
 ## Submit sensor data to server
 
 
-# Server (node.js)
+# Server (node.js RESTful API)
+    ### Administration Services
+        - [POST] Authenticate - get an admin token
+            parameters { u: <username>, p: <password> } (cleartext OK for now)
+            returns { token: <id>, timestamp: <stamp> }
+        - [POST] Calibrate - calibrate the dome positioning (ADMIN ONLY)
+            parameters { token: <id>, north: <alpha>, center: { x: <x>, y: <y>, z: <z> }
+        - [GET] Users - get a user list
+            parameters { token: <id> }
+            returns { users: [ 
+                        { token: <id>, useragent: <useragent>, email: <email>, location: {x,y,z} }, ... ]
+                        
+    ### User Services
+        - [POST] Authenticate - get a user token
+            parameters { useragent: <useragent>, email: <email address> }
+            returns { token: <id>, timestamp: <datetime> }
+        - [POST] Control - submit a control packet 
+            parameters { x: <x>, y: <y>, z: <z>, absolute: <absolute>, alpha: <alpha>, beta: <beta>, gamma: <gamma> }
+            returns { ? }
+        
