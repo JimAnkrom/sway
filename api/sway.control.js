00518755705d0b3c65f814071734da6dd3d1c6b4
JimAnkrom <arctanone@gmail.com>
2014-09-12 19:32:53 -0400

    Multi-User Initial Checkin

--- api/sway.control.js
+++ api/sway.control.js
@@ -10,7 +10,6 @@ module.exports = (function (){
 
     // Sway state management - leave private for now.
     sway.state = {
-        control: [],
         settings: {}
     };
 
@@ -21,7 +20,6 @@ module.exports = (function (){
             //console.log('Calibration Received - ' + JSON.stringify(data));
         },
         control: function (channel, control) {
-            sway.state.control.push(control);
             // we only care about the channel configuration, so use that
             this.processMotion(channel, control, 'rotation');
             this.processMotion(channel, control, 'orientation');
@@ -31,22 +29,18 @@ module.exports = (function (){
             if (cRoute) {
                 var cInput = control[inputType];
                 if (cInput) {
-                    if (cRoute.alpha != null) sway.server.send(cRoute.alpha, cInput.alpha);
-                    if (cRoute.beta != null) sway.server.send(cRoute.beta, cInput.beta);
-                    if (cRoute.gamma != null) sway.server.send(cRoute.gamma, cInput.gamma);
+                    sway.osc.send.apply(sway.osc, cRoute, cInput.alpha, cInput.beta, cInput.gamma);
                 }
             }
         },
         clear: function () {
-            sway.state.control = [];
             sway.state.settings = {};
         },
         debug: function () {
             return sway.state;
         },
-        send: function (address, value) {
-            // console.log(address + ' ' + value);
-            if (address) sway.osc.send(address, value);
+        send: function (address) {
+            if (address) sway.osc.send.apply(sway.osc, address, arguments);
             return null;
         },
         shutdown: function () {
