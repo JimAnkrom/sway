4ac8c59c1bdcca46668f88d021f9454fb232e88f
JimAnkrom <arctanone@gmail.com>
2014-09-21 11:36:12 -0400

    Closes #43 - alerts now inserted into the DOM.

--- ui/scripts/sway.input.js
+++ ui/scripts/sway.input.js
@@ -1,40 +1,14 @@
 /**
  * Created by Jim Ankrom on 7/30/2014.
  *
- * References:
- * http://www.w3.org/TR/orientation-event/
- * https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Orientation_and_motion_data_explained
- * http://diveintohtml5.info/geolocation.html
-
- * X, Y, Z - East, North, Up (off the device)
+ * sway.input should handle input from html control based input, such as clicks, sliders, knobs, etc.
  *
- * Device lying flat horizontal pointing west:
- *  {alpha: 90,
-       beta: 0,
-       gamma: 0};
-
- * touchstart, touchmove events
  */
 var sway = sway || {};
-sway.debugPanel = null;
-sway.templates = {
-    dataRow: function (label, value) {
-        return '<tr><td>' + label + '</td><td>' + value + '</td></tr>';
-    }
-};
-
-// Handle location events
-sway.location = {
-    get: function () {
-        navigator.geolocation.getCurrentPosition(this.handleGeolocation);
-    },
-    handleGeolocation: function (pos) {
-        sway.location.current = pos;
-    }
-};
 
 // Handle basic input
 sway.input = {
+    init: function () {},
     handleOSCClick: function (address, value) {
         if (sway.user) {
             if (sway.user.token) {
@@ -49,17 +23,51 @@ sway.input = {
             }
         }
     },
-    // TODO: touchstart, touchend, touchmove
-    handleTouchEvent: function () {
+    // Sliders are rotary knobs or fader / sliders.
+    handleSlider: function (sliderOptions, address, value) {
+
     }
 };
 
 
-// Class to handle all motion events
+/**
+ * Sway.motion - Created by Jim Ankrom on 9/14/2014.
+ *
+ * References:
+ * http://www.w3.org/TR/orientation-event/
+ * https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Orientation_and_motion_data_explained
+ * http://diveintohtml5.info/geolocation.html
+
+ * X, Y, Z - East, North, Up (off the device)
+ * Alpha - Yaw
+ * Beta - Pitch
+ * Gamma - Roll
+ *
+ * Alpha 0 is north; compass counts up counter-clockwise
+ *
+ * Device lying flat horizontal pointing west:
+ *  {alpha: 90, beta: 0, gamma: 0};
+ *
+ */
+// Plugin to handle all motion events
 sway.motion = {
+    init: function () {
+        // Detect Browser Capabilities and wire up events for each
+        if (window.DeviceOrientationEvent) {
+            sway.motion.capabilities.orientation = true;
+            window.addEventListener('deviceorientation', function (e) {
+                sway.motion.handleOrientationEvent.call(sway.motion, e);
+            });
+        }
+        if (window.DeviceMotionEvent) {
+            sway.motion.capabilities.motion = true;
+            window.addEventListener('devicemotion', function (e) {
+                sway.motion.handleMotionEvent.call(sway.motion, e);
+            });
+        }
+    },
     // TODO: Orientation
     // TODO: Motion
-    // TODO: Touch
     capabilities: {},
     calibration: {},
 
@@ -74,22 +82,6 @@ sway.motion = {
     // TODO: DeviceMotionEvent.interval
     // TODO: DeviceMotionEvent.rotationRate
     motion: null,
-    // TODO: Is this needed anymore with channel support?
-    handleMapOSC: function (address, value) {
-        if (sway.user) {
-            if (sway.user.token) {
-                var params = {
-                    token: sway.user.token,
-                    map: {
-                        channel: sway.user.token.channel || 'default',
-                        address: address,
-                        value: value
-                    }};
-                //directly post the click event
-                sway.api.post(sway.config.url + '/maposc', params, {});
-            }
-        }
-    },
     // DeviceOrientationEvent handler
     handleOrientationEvent: function (e) {
 //            if (!calibration) {
@@ -102,37 +94,26 @@ sway.motion = {
             sway.renderDebugEvent(sway.debugPanel, e);
         }
         sway.motion.calibration.orientation = e;
-        if (sway.user) {
-            if (sway.user.token) {
 
-                sway.motion.current = {token: sway.user.token, control: { orientation: {
-                    alpha: e.alpha, beta: e.beta, gamma: e.gamma, absolute: e.absolute
-                }}};
-                // set a value to compare to in setInterval closure
-                var timestamp = Date.now();
-                sway.motion.timestamp = timestamp;
+        sway.motion.current = { control: { orientation: {
+            alpha: e.alpha, beta: e.beta, gamma: e.gamma, absolute: e.absolute
+        }}};
 
-                // Setting a 50ms gate for now... faster later? Who knows. ;)
-                if (!sway.poll) {
-                    sway.poll = window.setInterval(function () {
-                        // if there has been no change in the timestamp, we are idle.
-                        if (sway.motion.timestamp == timestamp) {
-                            window.clearInterval(sway.poll);
-                            // setTimeout for idle expiration
-                            sway.idleTimeout = window.setTimeout(function () {
-                                sway.api.post(sway.config.url + sway.config.api.deleteUser, sway.motion.current, {});
-                            }, sway.config.user.idleTimeout);
-                            return;
-                        } else {
-                            // we are no longer idle, let's remove the idle timeout
-                            window.clearTimeout(sway.idleTimeout);
-                        }
-                        sway.api.post(sway.config.url + sway.config.api.control, sway.motion.current, {})
-                    }, sway.config.user.controlInterval);
+        // set a value to compare to in setInterval closure
+        var timestamp = Date.now();
+        sway.motion.timestamp = timestamp;
+
+        // Setting a 50ms gate for now... faster later? Who knows. ;)
+        if (!sway.poll) {
+            sway.poll = window.setInterval(function () {
+                // if there has been no change in the timestamp, we are idle
+                var isIdle = (sway.motion.timestamp == timestamp);
+                if (sway.motion.idle(isIdle)) {
+                    window.clearInterval(sway.poll);
+                    return;
                 }
-            }
-        } else {
-            alert('sway.user not set');
+                sway.api.post(sway.config.url + sway.config.api.control, sway.motion.current, {})
+            }, sway.config.user.controlInterval);
         }
     },
     // DeviceMotionEvent handler
@@ -144,71 +125,19 @@ sway.motion = {
         if (sway.debugPanel) {
             sway.renderDebugEvent.call(this, sway.debugPanel, e);
         }
+    },
+    idle: function (isIdle) {
+        if (isIdle) {
+            // setTimeout for idle expiration
+            sway.idleTimeout = window.setTimeout(function () {
+                sway.api.post(sway.config.url + sway.config.api.deleteUser, {}, {});
+            }, sway.config.user.idleTimeout);
+            return true;
+        }
+        // we are no longer idle, let's remove the idle timeout
+        window.clearTimeout(sway.idleTimeout);
+        return false;
     }
 };
-
-sway.renderDebugEvent = function (panel, e) {
-    var c = this.calibration;
-    if (!c) return;
-    var o = c.orientation,
-        m = c.motion,
-        r = c.rotation,
-        i = c.motionInterval,
-    //l = sway.location.current,
-        t = sway.templates;
-
-    var output = "<table>";
-    if (sway.user) {
-        output += t.dataRow('User Id', sway.user.token.uid);
-        output += t.dataRow('Channel', sway.user.channel.name);
-    }
-    if (o) {
-        output +=
-            t.dataRow('absolute', o.absolute)
-            + t.dataRow('alpha', o.alpha)
-            + t.dataRow('beta', o.beta)
-            + t.dataRow('gamma', o.gamma);
-    }
-    if (m) {
-        output +=
-            t.dataRow('accel.X', m.x)
-            + t.dataRow('accel.Y', m.y)
-            + t.dataRow('accel.Z', m.z);
-
-    }
-    if (r) {
-
-        output +=
-            t.dataRow('rot alpha', r.alpha)
-            + t.dataRow('rot beta', r.beta)
-            + t.dataRow('rot gamma', r.gamma);
-    }
-        //+ t.dataRow('interval', i)
-        //+ t.dataRow('latitude', l.latitude)
-        //+ t.dataRow('longitude', l.longitude)
-        //+ t.dataRow('altitude', l.altitude),
-        output += "</table>";
-    sway.debugPanel.innerHTML = output;
-};
-
-window.addEventListener('load', function () {
-    var element = document.getElementById('debugPanel');
-    if (element) {
-        sway.debugPanel = element;
-    }
-    // Detect Browser Capabilities
-    if (window.DeviceOrientationEvent) {
-        sway.motion.capabilities.orientation = true;
-        window.addEventListener('deviceorientation', function (e) {
-            sway.motion.handleOrientationEvent.call(sway.motion, e);
-        });
-    }
-    if (window.DeviceMotionEvent) {
-        sway.motion.capabilities.motion = true;
-        window.addEventListener('devicemotion', function (e) {
-            sway.motion.handleMotionEvent.call(sway.motion, e);
-        });
-    }
-});
 
 
--- ui/scripts/sway.user.js
+++ ui/scripts/sway.user.js
@@ -6,10 +6,55 @@
  * It requires the existence of global a config variable 'sway', which must have a value 'controlUrl'
  */
 var sway = sway || {};
-sway.oninitialized = null;
+// default config. Note that these values can be overwritten at any time by a message from sway
+sway.config = {
+    "url": 'http://192.168.1.100:1333',
+    "ui": {
+        // maximum alert messages allowed
+        "maxAlerts": 10
+    },
+    "user": {
+        // interval, in milliseconds, that motion control messages are sent
+        controlInterval: 50,
+        // time, in milliseconds, before an idle user times out
+        idleTimeout: 10000
+    },
+    "api": {
+        "users": "/users",
+        "control": "/control",
+        "osc": "/osc",
+        "deleteUser": "/delete"
+    },
+    update: function (config)  {
+        if (!config) return;
+        if (config.idleTimeout) sway.config.user.idleTimeout = config.idleTimeout;
+        if (config.controlInterval) sway.config.user.controlInterval = config.controlInterval;
+        if (config.serverUrl) sway.config.url = config.serverUrl;
+        if (config.api) sway.config.api = config.api;
+    }
+};
+
+sway.oninitialized = function () {
+    // initialize plugins here or override this
+    sway.motion.init();
+};
+sway.debugPanel = null;
+sway.outputPanel = null;
+sway.alertCount = 0;
+sway.templates = {
+    dataRow: function (label, value) {
+        return '<tr><td>' + label + '</td><td>' + value + '</td></tr>';
+    },
+    message: function (message) {
+        return '<div>' + message + '</div>';
+    }
+};
 sway.init = function () {
     if (!(sway.config.url)) return sway.debug('sway.config.url is not set!');
     sway.user.authorize();
+
+    sway.outputPanel = document.createElement('div');
+    document.body.appendChild(sway.outputPanel);
 };
 sway.debug = function (message) {
     if (console && console.log) {
@@ -17,17 +62,26 @@ sway.debug = function (message) {
     } else {
         sway.alert(message);
     }
-}
+};
 sway.alert = function (message) {
     if (message.isArray && message.isArray()) {
         for (var i=0; i < message.length; i++)
         {
-            window.alert(message[i]);
+            sway.addMessage(message);
         }
     }
     else
-        window.alert(message);
-}
+        sway.addMessage(message);
+};
+sway.addMessage = function (message){
+    sway.alertCount++;
+    if (sway.alertCount > sway.config.ui.maxAlerts) return;
+    var p = document.createElement('p');
+    var t = document.createTextNode(message);
+    p.appendChild(t);
+    sway.outputPanel.appendChild(p);
+
+};
 
 // Sway User Module
 sway.user = {
@@ -50,30 +104,6 @@ sway.user = {
     }
 };
 
-// default config. Note that these values can be overwritten at any time by a message from sway
-sway.config = {
-    "url": 'http://192.168.1.100:1333',
-    "user": {
-        // interval, in milliseconds, that motion control messages are sent
-        controlInterval: 50,
-        // time, in milliseconds, before an idle user times out
-        idleTimeout: 10000
-    },
-    "api": {
-        "users": "/users",
-        "control": "/control",
-        "osc": "/osc",
-        "deleteUser": "/delete"
-    },
-    update: function (config)  {
-        if (!config) return;
-        if (config.idleTimeout) sway.config.user.idleTimeout = config.idleTimeout;
-        if (config.controlInterval) sway.config.user.controlInterval = config.controlInterval;
-        if (config.serverUrl) sway.config.url = config.serverUrl;
-        if (config.api) sway.config.api = config.api;
-    }
-};
-
 // Sway API calls and utilities
 sway.api = {
     // Handles standard Sway Server return messages (non-request specific)
@@ -86,13 +116,23 @@ sway.api = {
 
         sway.config.update(data.config);
     },
+    addTokenToParams: function (params) {
+        if (sway.user) {
+            if (sway.user.token) {
+                params.token = sway.user.token;
+            }
+        }
+    },
     delete: function (url, params, options) {
+        sway.api.addTokenToParams(params);
         return sway.api.request(url, 'DELETE', params, options);
     },
     get: function (url, params, options) {
+        sway.api.addTokenToParams(params);
         return sway.api.request(url, 'GET', params, options);
     },
     post: function (url, params, options) {
+        sway.api.addTokenToParams(params);
         return sway.api.request(url, 'POST', params, options);
     },
     request: function (url, verb, params, options) {
@@ -139,3 +179,54 @@ sway.api = {
         return sway.debug('You are using a browser that does not support required technology for Sway!');
     }
 };
+
+sway.renderDebugEvent = function (panel, e) {
+    var c = this.calibration;
+    if (!c) return;
+    var o = c.orientation,
+        m = c.motion,
+        r = c.rotation,
+        i = c.motionInterval,
+    //l = sway.location.current,
+        t = sway.templates;
+
+    var output = "<table>";
+    if (sway.user) {
+        output += t.dataRow('User Id', sway.user.token.uid);
+        output += t.dataRow('Channel', sway.user.channel.name);
+    }
+    if (o) {
+        output +=
+            t.dataRow('absolute', o.absolute)
+            + t.dataRow('alpha', o.alpha)
+            + t.dataRow('beta', o.beta)
+            + t.dataRow('gamma', o.gamma);
+    }
+    if (m) {
+        output +=
+            t.dataRow('accel.X', m.x)
+            + t.dataRow('accel.Y', m.y)
+            + t.dataRow('accel.Z', m.z);
+
+    }
+    if (r) {
+
+        output +=
+            t.dataRow('rot alpha', r.alpha)
+            + t.dataRow('rot beta', r.beta)
+            + t.dataRow('rot gamma', r.gamma);
+    }
+    //+ t.dataRow('interval', i)
+    //+ t.dataRow('latitude', l.latitude)
+    //+ t.dataRow('longitude', l.longitude)
+    //+ t.dataRow('altitude', l.altitude),
+    output += "</table>";
+    sway.debugPanel.innerHTML = output;
+};
+
+window.addEventListener('load', function () {
+    var element = document.getElementById('debugPanel');
+    if (element) {
+        sway.debugPanel = element;
+    }
+});
