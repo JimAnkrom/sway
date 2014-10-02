a3268f4e9a321a63b9384fed847ddbc00db760b5
JimAnkrom <arctanone@gmail.com>
2014-10-01 20:02:48 -0400

    Channel unit tests

---
+++ test/scripts/api/sway.channels.test.js
@@ -0,0 +1,145 @@
+/**
+ * Created by Jim Ankrom on 9/10/2014.
+ */
+var swayChannels = require('../../../api/sway.channels.js');
+
+var testChannels = {
+    "TESTCHANNEL": {
+        "address": "192.168.1.202",
+        "port": 6000,
+        "displayName": "Multi-user test channel 3",
+        "description": "This channel is the PINK CUBE",
+        "orientation": "/orient/3"
+    }
+};
+var oldChannelConfig = swayChannels.channels;
+
+function TestChannel () {
+    this.users = [];
+};
+
+exports.tests = {
+
+    setUp: function (callback) {
+        //swayChannels.channels = testChannels;
+        callback();
+    },
+
+    tearDown: function (callback) {
+        swayChannels.channels = oldChannelConfig;
+        callback();
+    },
+
+    // test init
+    //Channels_Init: function (test) {},
+
+
+    // test enqueue
+    Channels_Enqueue: function (test) {
+        var testUser = {};
+        var testChannel = new TestChannel();
+
+        swayChannels.enqueue(testChannel, testUser);
+
+        test.ok(testUser.channel != null);
+        test.ok(testUser.channel === testChannel);
+        test.done();
+    },
+
+    // test dequeue
+    Channels_Dequeue: function (test) {
+        // Arrange
+        var testChannel = new TestChannel();
+        var testUser = { uid: 123 };
+        swayChannels.enqueue(testChannel, testUser);
+        test.ok(testUser.active);
+        test.ok(testUser.channel != null);
+        test.ok(testUser.channel === testChannel);
+
+        // Act
+        swayChannels.remove(testChannel, testUser);
+
+        test.done();
+    },
+
+    // test remove
+    Channels_Remove: function (test) {
+        // Arrange
+        var testChannel = new TestChannel();
+        var testUser = { uid: 123 };
+        swayChannels.enqueue(testChannel, testUser);
+        test.ok(testUser.active);
+        test.ok(testUser.channel != null);
+        test.ok(testUser.channel === testChannel);
+
+        // Act
+        swayChannels.remove(testChannel, testUser);
+
+        // Assert
+        // User should not be active
+        test.ok(!testUser.active);
+        // User should not have a channel
+        test.ok(testUser.channel == null);
+        // No users should be left behind
+        test.ok(testChannel.users.length == 0);
+
+        test.done();
+    },
+
+    // test update
+    Channels_Update: function (test) {
+        // Arrange
+        var testUser = { uid: 123 };
+
+        swayChannels.update(testUser);
+
+        test.ok(testUser.active);
+        test.ok(testUser.channel != null);
+
+        test.done();
+    },
+
+    Channels_Update_DoesNotChangeUsersExistingChannel: function (test) {
+        var testChannel = new TestChannel();
+        var testUser = { uid: 123 };
+        swayChannels.enqueue(testChannel, testUser);
+        test.ok(testUser.active);
+        test.ok(testUser.channel != null);
+        test.ok(testUser.channel === testChannel);
+
+        // Act
+        swayChannels.update(testUser);
+
+        test.ok(testUser.active);
+        test.ok(testUser.channel != null);
+        test.ok(testUser.channel === testChannel);
+
+        test.done();
+    },
+    Channels_Reassign_ChangesUsersExistingChannel: function (test) {
+        var testChannel = new TestChannel();
+        testChannel.name = "TEST CHANNEL ONE";
+        var testUser = { uid: 123 };
+        swayChannels.enqueue(testChannel, testUser);
+        test.ok(testUser.active);
+        test.ok(testUser.channel != null);
+        test.ok(testUser.channel === testChannel);
+
+        // Act
+        swayChannels.reassign(testUser);
+
+        test.ok(testUser.active);
+        test.ok(testUser.channel != null);
+        test.ok(testUser.channel != testChannel);
+        test.ok(testUser.channel.name != testChannel.name);
+        console.log(testUser.channel.name);
+        test.done();
+    }
+
+    // test test assign
+    // test reassign
+    // test compact
+    //Channels_Compact: function () {}
+
+    // test load balancers
+};
